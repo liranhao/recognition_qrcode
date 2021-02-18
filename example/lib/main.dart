ecfe87393f69c3d3debf53d3f60bf9b2bc0ae19c
limouren <liranhao@sfmail.sf-express.com>
2021-02-18 19:03:32 +0800

    支持安卓7以下系统

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:recognition_qrcode/recognition_qrcode.dart';
 
@@ -24,14 +23,6 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await RecognitionQrcode.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
     // setState to update our non-existent appearance.
