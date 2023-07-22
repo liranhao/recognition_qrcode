1820a8ad652bf7e0acb18a1d44a392b0432bcf46
limouren <1632207097@qq.com>
2023-07-22 16:53:26 +0800

    git action

--- test/recognition_qrcode_test.dart
+++ test/recognition_qrcode_test.dart
@@ -3,19 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:recognition_qrcode/recognition_qrcode.dart';
 
 void main() {
-  // const MethodChannel channel = MethodChannel('recognition_qrcode');
+  const MethodChannel channel = MethodChannel('recognition_qrcode');
 
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  // setUp(() {
-  //   channel.setMockMethodCallHandler((MethodCall methodCall) async {
-  //     return '42';
-  //   });
-  // });
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      return '42';
+    });
+  });
 
-  // tearDown(() {
-  //   channel.setMockMethodCallHandler(null);
-  // });
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
 
   test('getPlatformVersion', () async {
     expect(await RecognitionQrcode.platformVersion, '42');
